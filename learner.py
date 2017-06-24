fa0080f01beeb7287bbd967ef133a414635e7eac
Marek Rydlewski <marek.rydlewski@outlook.com>
2017-06-24 02:09:39 +0200

    --amend

--- learner.py
+++ learner.py
@@ -6,6 +6,9 @@ import numpy as np
 __threshold = 120
 __is_road = np.array([1, 0])
 __is_not_road = np.array([0, 1])
+__windowsize_r = 20
+__windowsize_c = 20
+
 
 def load_img(name):
     img = ndimage.imread(name)
@@ -13,13 +16,27 @@ def load_img(name):
 
 
 def slice_image(img):
-    windowsize_r = 20
-    windowsize_c = 20
     windows = []
-    for r in range(0, img.shape[0] - windowsize_r, windowsize_r):
-        for c in range(0, img.shape[1] - windowsize_c, windowsize_c):
-            windows.append(img[r:r + windowsize_r, c:c + windowsize_c])
+    for r in range(0, img.shape[0] - __windowsize_r, __windowsize_r):
+        for c in range(0, img.shape[1] - __windowsize_c, __windowsize_c):
+            windows.append(img[r:r + __windowsize_r, c:c + __windowsize_c])
     return windows
+
+
+def get_data_from_images(img_map, img_sat):
+    slices_map = slice_image(img_map)
+    slices_sat = slice_image(img_sat)
+    x_train = np.zeros(len(slices_map), 20, 20, 3)
+    y_train = np.zeros(len(slices_sat), 2)
+
+    for i, (slice_map, slice_sat) in enumerate(zip(slices_map, slices_sat)):
+        if np.sum(slice_map >__threshold) >= 1:
+            y_train[i] = __is_road
+            x_train[i] = slice_sat
+        else:
+            y_train[i] = __is_not_road
+            x_train[i] = slices_sat
+    return (x_train, y_train)
 
 
 def learn_directory():
@@ -34,15 +51,4 @@ def learn_directory():
 if __name__ == "__main__":
     img_map = load_img("test/map/10378780_15.tif")
     img_sat = load_img("test/sat/10378780_15.tiff")
-
-    slices_map = slice_image(img_map)
-    slices_sat = slice_image(img_sat)
-    x_train = np.zeros(len(slices_map), 20, 20, 3)
-    y_train = np.zeros(len(slices_sat), 2)
-    for i, (slice_map, slice_sat) in enumerate(zip(slices_map, slices_sat)):
-        if np.sum(slice_map >__threshold) >= 1:
-            y_train[i] = __is_road
-            x_train[i] = slice_sat
-        else:
-            y_train[i] = __is_not_road
-            x_train[i] = slices_sat
+    x, y = get_data_from_images(img_map, img_sat)
