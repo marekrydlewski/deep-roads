9b4fa23ccd6d4016fc05fa9c3dd8971b26b60a34
Marek Rydlewski <marek.rydlewski@outlook.com>
2017-06-24 02:05:17 +0200

    Create data for keras from slices

--- learner.py
+++ learner.py
@@ -1,7 +1,11 @@
 # import neural
 from scipy import ndimage, misc
 import os
+import numpy as np
 
+__threshold = 120
+__is_road = np.array([1, 0])
+__is_not_road = np.array([0, 1])
 
 def load_img(name):
     img = ndimage.imread(name)
@@ -30,6 +34,15 @@ def learn_directory():
 if __name__ == "__main__":
     img_map = load_img("test/map/10378780_15.tif")
     img_sat = load_img("test/sat/10378780_15.tiff")
+
     slices_map = slice_image(img_map)
     slices_sat = slice_image(img_sat)
-
+    x_train = np.zeros(len(slices_map), 20, 20, 3)
+    y_train = np.zeros(len(slices_sat), 2)
+    for i, (slice_map, slice_sat) in enumerate(zip(slices_map, slices_sat)):
+        if np.sum(slice_map >__threshold) >= 1:
+            y_train[i] = __is_road
+            x_train[i] = slice_sat
+        else:
+            y_train[i] = __is_not_road
+            x_train[i] = slices_sat
